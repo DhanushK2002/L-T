204b3ae563e97ce011391e0af5295fdfaafa2b7b
kirann1234 <kirangowda321chn@gmail.com>
2025-04-15 14:21:03 +0530

    Update from kiran

--- LNT.java
+++ LNT.java
@@ -15,6 +15,7 @@ public class LNT {
      */
     public static void main(String[] args) {
         System.out.println("Hello This is Team 3");
+         System.out.println("Github project");
         // TODO code application logic here
     }
     
