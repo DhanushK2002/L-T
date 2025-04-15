a89ff2c6c7d82516b91722284c64b76210187d52
DhanushK2002 <dhanush23702@gmail.com>
2025-04-15 11:06:47 +0530

    Add files via upload

---
+++ LNT.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lnt;
+
+/**
+ *
+ * @author INTEL
+ */
+public class LNT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello This is Dhansuh");
+        // TODO code application logic here
+    }
+    
+}
